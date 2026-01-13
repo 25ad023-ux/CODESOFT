2a9cb85d62ba39b20cdc53010f6baa3c99c1fb8a
25ad023-ux <25ad023@rmkec.ac.in>
2026-01-13 17:46:44 +0530

    initial commit

---
+++ Task2.py
@@ -0,0 +1,77 @@
+import random
+
+user = input("Enter rock, paper or scissors: ")
+computer = random.choice(["rock", "paper", "scissors"])
+
+print("You:", user)
+print("Computer:", computer)
+
+if user == computer:
+    print("Tie")
+elif (user == "rock" and computer == "scissors") or \
+     (user == "scissors" and computer == "paper") or \
+     (user == "paper" and computer == "rock"):
+    print("You win")
+else:
+    print("You lose")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
